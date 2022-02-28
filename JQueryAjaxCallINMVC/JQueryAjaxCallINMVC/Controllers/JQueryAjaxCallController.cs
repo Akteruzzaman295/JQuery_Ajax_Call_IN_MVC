d39bca7fef690bac6c90b9e67e296a0ce9fad4d2
Akteruzzaman295 <akteruzzaman4292@gmail.com>
2022-02-28 22:11:12 +0600

    JQuery Ajax call in MVC

---
+++ JQueryAjaxCallINMVC/JQueryAjaxCallINMVC/Controllers/JQueryAjaxCallController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQueryAjaxCallINMVC.Controllers
+{
+    public class JQueryAjaxCallController : Controller
+    {
+        //
+        // GET: /JQueryAjaxCall/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult AjaxPostCall(Employee employeeData)
+        {
+            Employee employee = new Employee
+            {
+                Name = employeeData.Name,
+                Designation = employeeData.Designation,
+                Location = employeeData.Location
+            };
+            return Json(employee,JsonRequestBehavior.AllowGet);
+        }
+
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ JQueryAjaxCallINMVC/JQueryAjaxCallINMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JQueryAjaxCallINMVC.MvcApplication" Language="C#" %>
